708be88094a9d10dc23e255372bc1945fbca5978
임왕성 <76050934+ws3494@users.noreply.github.com>
2023-03-28 22:45:14 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    answer += 1;
+    while (answer % 3 === 0 || answer.toString().includes('3')) {
+      answer += 1;
+    }
+  }
+  return answer;
+}
